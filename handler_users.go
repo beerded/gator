5fdb2a78acc8578c33992b48468b0c41cce35568
A P <67877471+beerded@users.noreply.github.com>
2026-03-25 16:30:39 +0200

    Add function to list users in database

---
+++ handler_users.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerUsers(s *state, cmd command) error {
+
+	ctx := context.Background()
+	users, err := s.db.GetUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("Error getting users: %w", err)
+	}
+
+	currentUser := s.cfg.CurrentUserName
+
+	fmt.Println("USERS:")
+	if len(users) == 0 {
+		fmt.Println("Database empty. No users to show")
+		return nil
+	}
+
+	for _, user := range users {
+		toPrint := fmt.Sprintf("* %s", user)
+		if user == currentUser {
+			toPrint += fmt.Sprintf(" (current)")
+		}
+		toPrint += "\n"
+		fmt.Printf(toPrint)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func main() {
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerUsers)
 
 	commandStruct := command{name: args[0], args: args[1:]}
 
